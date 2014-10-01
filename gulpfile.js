d31120f7eb955fc1d6c00a64f891d6b377a2333e
Francisco Serrano <fserrano@gmail.com>
2014-09-30 22:42:29 -0400

    Improve gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,44 +1,18 @@
 var gulp = require('gulp'),
     uglify = require('gulp-uglify'),
-    // concat = require('gulp-concat'),
     rename = require('gulp-rename'),
-    // clean = require('gulp-clean'),
     livereload = require('gulp-livereload');
 
-gulp.task('compress', function() {
+gulp.task('compress',function() {
   return gulp.src('src/taxonomy-jquery.js')
-    .pipe(uglify())
-    .pipe(gulp.dest('dist'))
-    .pipe(rename('taxonomy.min.js'));
-});
-/*
-gulp.task('compress', function() {
-  return gulp.src(['src/taxonomy.js','src/plugin.js'])
-    .pipe(concat('concat.js'))
-    .pipe(gulp.dest('dist'))
     .pipe(rename('taxonomy.min.js'))
-    .pipe(uglify())
-    .pipe(gulp.dest('dist'));
+    .pipe(gulp.dest('dist'))
+    .pipe(uglify({mangle: true,compress:true}));
 });
-
-gulp.task('remove-concat', ['compress'], function(){
-    return gulp.src('dist/concat.js')
-               .pipe(clean());
-});
-
-gulp.task('watch', ['remove-concat'],function() {
-  gulp.watch('src/*.js', ['compress']);
+gulp.task('watch',function() {
   var server = livereload();
-  gulp.watch(['src/*.js','example.html']).on('change', function(file) {
-      server.changed(file.path);
-  });
-});
-*/
-gulp.task('watch',['compress'], function() {
-  var server = livereload();
-  gulp.watch(['src/*.js','example.html']).on('change', function(file) {
+  gulp.watch(['src/*.js','example.html'],['compress']).on('change', function(file) {
       server.changed(file.path);
   });
 });
 gulp.task('default', ['watch','compress']);
-
